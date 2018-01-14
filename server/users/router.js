1eeb8a41e4ccd49874ff9cf1962970d07406ff75
daviddinnison <david.g.dinnison@gmail.com>
2018-01-14 16:26:07 -0500

    backend fetch for user locations to be converted into protected data endpoint

--- server/users/router.js
+++ server/users/router.js
@@ -144,13 +144,25 @@ router.get('/', (req, res) => {
     .catch(err => res.status(500).json({message: 'Internal server error'}));
 });
 
+// router.get('/:id', jsonParser, (req, res) => {
+//   console.log(req.params.id)
+//   User
+//       .findById(req.params.id)
+//       .then(user => {
+//           if (!user) { return res.status(404).end(); }
+//           return res.status(200).json(user);
+//       })
+//       .catch(err => next(err));
+// });
+
+//locations
 router.get('/:id', jsonParser, (req, res) => {
   console.log(req.params.id)
   User
       .findById(req.params.id)
       .then(user => {
           if (!user) { return res.status(404).end(); }
-          return res.status(200).json(user);
+          return res.status(200).json(user.locations);
       })
       .catch(err => next(err));
 });
