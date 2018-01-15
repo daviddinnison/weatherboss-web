dac34eb86a709763052447a9698c59f0094ec705
daviddinnison <david.g.dinnison@gmail.com>
2018-01-15 16:13:33 -0500

    working extended forecast, might change to different type

--- client/src/actions/forecast.js
+++ client/src/actions/forecast.js
@@ -1,11 +1,14 @@
+const API_KEY = "b20a7be72cb0b77a";
+
 export const GET_EXTENDED_FORECAST_REQUEST = 'GET_EXTENDED_FORECAST_REQUEST';
 export const getExtendedForecastRequest = () => ({
     type: GET_EXTENDED_FORECAST_REQUEST,
 });
 
 export const GET_EXTENDED_FORECAST_SUCCESS = 'GET_EXTENDED_FORECAST_SUCCESS';
-export const getExtendedForecastSuccess = locations => ({
+export const getExtendedForecastSuccess = data => ({
     type: GET_EXTENDED_FORECAST_SUCCESS,
+    data
 });
 
 export const GET_EXTENDED_FORECAST_ERROR = 'GET_EXTENDED_FORECAST_ERROR';
@@ -16,7 +19,7 @@ export const getExtendedForecastError = message => ({
 
 export const getExtendedForecast = (userInput) => dispatch => {
     dispatch(getExtendedForecastRequest());
-    fetch(`http://api.wunderground.com/api/b20a7be72cb0b77a/conditions/q/${userInput}.json`, {})
+    fetch(`http://api.wunderground.com/api/${API_KEY}/hourly10day/q/${userInput}.json`, {})
         .then(res => {
             console.log('INSIDE FIRST .THEN. this is the response:::', res)
             if (!res.ok) {
@@ -26,8 +29,8 @@ export const getExtendedForecast = (userInput) => dispatch => {
             return res.json();
         })
         .then(data => {
-            console.log(data, 'data received from server-----')
-            dispatch(getExtendedForecastSuccess(data));
+            console.log(data.hourly_forecast, 'data received from server-----')
+            dispatch(getExtendedForecastSuccess(data.hourly_forecast));
         })
 
         .catch(err => {
