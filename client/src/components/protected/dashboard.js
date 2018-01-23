aa8deb076de53fbefd6da941b4152bab13148166
daviddinnison <david.g.dinnison@gmail.com>
2018-01-22 22:52:02 -0500

    addlocation hover

--- client/src/components/protected/dashboard.js
+++ client/src/components/protected/dashboard.js
@@ -1,43 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-import { connect } from 'react-redux';
-import requiresLogin from '../requires-login';
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import requiresLogin from "../requires-login";
 
-import { clearRedirect } from '../../actions/protected-data';
+import { clearRedirect } from "../../actions/protected-data";
 
-import Locations from './locations';
+import Locations from "./locations";
 
-import './styles/dashboard.css';
+import "./styles/dashboard.css";
 
 export class Dashboard extends React.Component {
-    componentDidMount() {
-        //if user added a location this clears the reducer of redirection props so the user can add another location
-        if (this.props.redirect) {
-            this.props.dispatch(clearRedirect())
-        }
+  componentDidMount() {
+    //if user added a location this clears the reducer of redirection props so the user can add another location
+    if (this.props.redirect) {
+      this.props.dispatch(clearRedirect());
     }
-    render() {
-        return (
-            <div className="dashboard container">
-                <div className="dashboard-username">
-                    Welcome, {this.props.username}
-                </div>
-                <Locations />
-                <div className="dashboard-add-location">
-                    <Link to="/newlocation">Add location</Link>
-                </div>
-            </div>
-        );
-    }
+  }
+  render() {
+    return (
+      <div className="dashboard container">
+        <div className="dashboard-username">Welcome, {this.props.username}</div>
+        <Locations />
+        <Link to="/newlocation" className="dashboard-add-location">
+          Add location
+        </Link>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    const { currentUser } = state.auth;
-    return {
-        username: state.auth.currentUser.username,
-        name: `${currentUser.firstName} ${currentUser.lastName}`,
-        redirect: state.protectedData.redirect
-    };
+  const { currentUser } = state.auth;
+  return {
+    username: state.auth.currentUser.username,
+    name: `${currentUser.firstName} ${currentUser.lastName}`,
+    redirect: state.protectedData.redirect
+  };
 };
 
 export default requiresLogin()(connect(mapStateToProps)(Dashboard));
